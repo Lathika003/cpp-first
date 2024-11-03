2141c267122734a5a636bca74efd0df73a431dbf
Lathika003 <145969120+Lathika003@users.noreply.github.com>
2024-11-04 00:27:55 +0530

    get user inputs and output them
    get name and age as user inputs and output a single sentence combining the user inputs

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+    cout<<"hello "<<name<<endl;
+
+    cout<<"you are"<<age<<"years old";
+    return 0;
+}
