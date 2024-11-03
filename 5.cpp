3d539d69b57d6b29cabe5f0a1bcf117011f521b4
Lathika003 <145969120+Lathika003@users.noreply.github.com>
2024-11-04 00:29:12 +0530

    get user inputs and output them
    get 2 user inputs and output them in a one sentence

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string city,country;
+    cout<<"Enter the city you live: ";
+    cin>>city;
+    cout<<"Enter the country that you live: ";
+    cin>>country;
+    cout<<"You live in "<<city<<","<<country;
+    return 0;
+}
